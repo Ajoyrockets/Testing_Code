1c086c8def8e2e923f5b8192f6e9c4b7946e2219
Ajoyrockets <ajoypartapsingh@gmail.com>
2024-11-04 16:49:27 +0000

    modified:   .vscode/settings.json 	modified:   Code(C,C++,C#)/Learning_c++.cpp 	modified:   Pygame_testing_code_file/Pygame1/First_pygame_testing_code_python_file.py

--- Pygame_testing_code_file/Pygame1/First_pygame_testing_code_python_file.py
+++ Pygame_testing_code_file/Pygame1/First_pygame_testing_code_python_file.py
@@ -13,14 +13,24 @@ GAME_FILE_LOCATION = os.path.dirname(__file__)
 IMAGES = os.path.join(GAME_FILE_LOCATION,'Assets', 'Images')
 SOUNDS = os.path.join(GAME_FILE_LOCATION,'Assets', 'Sounds')
 
+
+
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join(IMAGES, 'spaceship_yellow.png'))
+YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE,(60,40)),270)
+
+
 RED_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join(IMAGES, 'spaceship_red.png'))
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE,(60,40)),90)
+
 
 def draw_window():
     WIN.fill((WHITE))
-    WIN.blit(YELLOW_SPACESHIP_IMAGE,(300,100))
+    WIN.blit(YELLOW_SPACESHIP_IMAGE,(500,100))
+    WIN.blit(RED_SPACESHIP_IMAGE,(100,100))
     pygame.display.update()
 
 
